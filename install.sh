95723b92678a0ee36188e0ac63758e80c2eabeb8
ikuamike <5783614+ikuamike@users.noreply.github.com>
2020-07-06 19:41:04 +0300

    changed echo --> printf

--- install.sh
+++ install.sh
@@ -25,86 +25,86 @@ BWhite='\033[1;37m'       # White
 
 ZSH_CUSTOM=${HOME}/.oh-my-zsh/custom
 
-echo -e "\n${BWhite}[*] Setting up your terminal... ${Color_Off}"
+printf "\n${BWhite}[*] Setting up your terminal... ${Color_Off}\n"
 
-echo -e "\n${BGreen}[*] Installing dependencies... ${Color_Off}\n"
+printf "\n${BGreen}[*] Installing dependencies... ${Color_Off}\n"
 
 if ! command -v curl &> /dev/null
 then
-	echo -e "${Red}[-] curl could not be found ${Color_Off}"
-	echo -e "${Yellow}[+] Installing curl... ${Color_Off}\n"
+	printf "${Red}[-] curl could not be found ${Color_Off}\n"
+	printf "${Yellow}[+] Installing curl... ${Color_Off}\n"
 	sudo apt install curl -y    
 else
-	echo -e "${Green}[+] curl already installed...skipping ${Color_Off}"
+	printf "${Green}[*] curl already installed...skipping ${Color_Off}\n"
 fi
 
 if ! command -v git &> /dev/null
 then
-	echo -e "${Red}[-] git could not be found ${Color_Off}"
-	echo -e "${Yellow}[+] Installing git... ${Color_Off}\n"
+	printf "${Red}[-] git could not be found ${Color_Off}\n"
+	printf "${Yellow}[+] Installing git... ${Color_Off}\n"
 	sudo apt install git -y
 else
-	echo -e "${Green}[*] git already installed...skipping ${Color_Off}"
+	printf "${Green}[*] git already installed...skipping ${Color_Off}\n"
 fi
 
 if ! command -v tmux &> /dev/null
 then
-	echo -e "${Red}[-] tmux could not be found ${Color_Off}"
-	echo -e "${Yellow}[+] Installing tmux... ${Color_Off}\n"
+	printf "${Red}[-] tmux could not be found ${Color_Off}\n"
+	printf "${Yellow}[+] Installing tmux... ${Color_Off}\n"
 	sudo apt install tmux -y  
 else
-	echo -e "${Green}[*] tmux already installed...skipping ${Color_Off}"
+	printf "${Green}[*] tmux already installed...skipping ${Color_Off}\n"
 fi
 
 if ! command -v zsh &> /dev/null
 then
-	echo -e "\n${Red}[-] zsh could not be found ${Color_Off}"
-	echo -e "${Yellow}[+] Installing zsh... ${Color_Off}\n"
+	printf "\n${Red}[-] zsh could not be found ${Color_Off}\n"
+	printf "${Yellow}[+] Installing zsh... ${Color_Off}\n"
 	sudo apt install zsh -y   
 else
-	echo -e "${Green}[*] zsh already installed...skipping ${Color_Off}"
+	printf "${Green}[*] zsh already installed...skipping ${Color_Off}\n"
 fi
 
-echo -e "\n${Yellow}[+] Installing Powerline fonts... ${Color_Off}\n"
+printf "\n${Yellow}[+] Installing Powerline fonts... ${Color_Off}\n"
 git clone --quiet https://github.com/powerline/fonts.git --depth=1 /tmp/fonts
 chmod +x /tmp/fonts/install.sh
 /tmp/fonts/install.sh
 rm -rf /tmp/fonts
 
 if [ ! -d ${HOME}/.oh-my-zsh ]; then
-	echo -e "${Yellow}[+] Installing ohmyzsh... ${Color_Off}\n"
+	printf "${Yellow}[+] Installing ohmyzsh... ${Color_Off}\n"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 else
-	echo -e "\n${Green}[*] ohmyzsh already installed...skipping${Color_Off}"
+	printf "\n${Green}[*] ohmyzsh already installed...skipping${Color_Off}\n"
 fi
 
-echo -e "${Blue}[+] Configuring zsh: ${White}Creating zshrc symlink to ~/.zshrc ... ${Color_Off}\n"
+printf "${Blue}[+] Configuring zsh: ${White}Creating zshrc symlink to ~/.zshrc ... ${Color_Off}\n"
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 
 if [ ! -d ${ZSH_CUSTOM}/themes/powerlevel10k ]; then
-	echo -e "\n${Yellow}[+] Installing powerlevel10k... ${Color_Off}"
+	printf "\n${Yellow}[+] Installing powerlevel10k... ${Color_Off}\n"
 	git clone --quiet --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k
 else
-	echo -e "${Green}[*] powerlevel10k already installed...skipping${Color_Off}"
+	printf "${Green}[*] powerlevel10k already installed...skipping${Color_Off}\n"
 fi
 
 if [ ! -d ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting ]; then
-	echo -e "\n${Yellow}[+] Installing zsh-syntax-highlighting... ${Color_Off}\n"
+	printf "\n${Yellow}[+] Installing zsh-syntax-highlighting... ${Color_Off}\n"
 	git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
 else
-	echo -e "${Green}[*] zsh-syntax-highlighting already installed...skipping${Color_Off}"
+	printf "${Green}[*] zsh-syntax-highlighting already installed...skipping${Color_Off}\n"
 fi
 
 if [ ! -d ~/.fzf ]; then
-	echo -e "\n${Yellow}[+] Installing fzf... ${Color_Off}\n"
+	printf "\n${Yellow}[+] Installing fzf... ${Color_Off}\n"
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 	sed -i 's/\<curl\>/curl -s/g' ~/.fzf/install
 	~/.fzf/install --no-bash --no-fish --key-bindings --completion --no-update-rc
 else
-	echo -e "${Green}[*] fzf already installed...skipping${Color_Off}"
+	printf "${Green}[*] fzf already installed...skipping${Color_Off}\n"
 fi
 
-echo -e "\n${Green}[+] Setting up tmux... ${Color_Off}\n"
+printf "\n${Green}[+] Setting up tmux... ${Color_Off}\n"
 ${HOME}/dotfiles/setup-tmux.sh
 
 if [ "$(basename "$SHELL")" != "zsh" ]; then
