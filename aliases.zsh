5ba0813cf9563cde269fdb763759d91a742977fd
ikuamike <5783614+ikuamike@users.noreply.github.com>
2020-08-31 01:06:07 +0300

    add custom aliases

---
+++ aliases.zsh
@@ -0,0 +1,12 @@
+# Docker alias
+alias dk='docker'
+alias dkps="docker ps"
+alias dkst="docker stats"
+alias dkpsa="docker ps -a"
+alias dki="docker images"
+alias dkrm="docker rm"
+alias dkrmi="docker rmi"
+alias dkcpup="docker-compose up -d"
+alias dkcpdown="docker-compose down"
+alias dkcpstart="docker-compose start"
+alias dkcpstop="docker-compose stop"
